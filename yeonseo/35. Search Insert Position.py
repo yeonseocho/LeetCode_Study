558c92f8c8f4bf03ca74e943a5a3fac71e539440
Yeonseo <103731501+yeonseocho@users.noreply.github.com>
2022-07-20 18:35:48 +0900

    Create 35. Search Insert Position.py

---
+++ yeonseo/35. Search Insert Position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        # nums(Ascending order)
+        left=0
+        right=len(nums)-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]>target: # Search to left
+                right=mid-1
+            elif nums[mid]<target: # Search to right
+                left=mid+1
+            else: # Correct
+                return mid
+        # Incorrect
+        return left
